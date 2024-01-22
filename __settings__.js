c27af4d087c97b3432593889864f5bd4ea475cf4
Mystical <sakshamsaini123@gmail.com>
2024-01-22 11:03:15 +0530

    first push

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1937205.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "high-performance"
+};
+window.SCRIPTS = [ 164412118, 164412119, 164412142, 164412122, 164412123, 164412124, 164412125, 164412127, 164412128, 164412129, 164412130, 164412131, 164412133, 164412134 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/164412139/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/164412140/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/164412138/1/ammo.js', 'preload' : true},
+];
